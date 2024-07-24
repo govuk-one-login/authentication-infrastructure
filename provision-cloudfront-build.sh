9bd79cec314bd0b8a08d14a0ce06afc36a920d46
Monjurul Haque <monjurul.haque@digital.cabinet-office.gov.uk>
2024-07-24 10:56:31 +0100

    Provision cloudfront-monitoring-alarm stack
    Issue: AUT-3365

--- provision-cloudfront-build.sh
+++ provision-cloudfront-build.sh
@@ -8,10 +8,17 @@ export AWS_ACCOUNT=di-authentication-build
 export AWS_PROFILE=di-authentication-build-AWSAdministratorAccess
 aws sso login --profile "${AWS_PROFILE}"
 
-source "scripts/read_secrets.sh" "build"
-source "scripts/read_parameters.sh" "build"
+# shellcheck source=/dev/null
+source "./scripts/read_secrets.sh" "build"
 
-# cloudfront certificate generation, no dependency
+# shellcheck source=/dev/null
+source "./scripts/read_parameters.sh" "build"
+
+# ----------------------------------------------------------
+# auth-fe-cloudfront-certificate
+#   Provision an ACM certificate for CloudFront distribution
+#   no dependency
+# ----------------------------------------------------------
 PARAMETERS_FILE="configuration/$AWS_ACCOUNT/auth-fe-cloudfront-certificate/parameters.json"
 HostedZoneID=${signin_route53_hostedzone_id:-""}
 PARAMETERS=$(jq ". += [{\"ParameterKey\":\"HostedZoneID\",\"ParameterValue\":\"${HostedZoneID}\"}] | tojson" -r "${PARAMETERS_FILE}")
@@ -26,7 +33,11 @@ CertificateARN=$(aws cloudformation describe-stacks \
     --stack-name auth-fe-cloudfront-certificate --region us-east-1  |
     jq -r '.Stacks[0].Outputs[] | select(.OutputKey | match("CertificateARN")) | .OutputValue')
 
-# cloudfront distribution, dependent on auth-fe-cloudfront-certificate
+# ------------------------------------------------------------------------------
+# auth-fe-cloudfront
+#   Creates the CloudFront distribution
+#   depends on: auth-fe-cloudfront-certificate
+# ------------------------------------------------------------------------------
 PARAMETERS_FILE="configuration/$AWS_ACCOUNT/auth-fe-cloudfront/parameters.json"
 OriginCloakingHeader=${signin_origin_cloaking_header:-""}
 PreviousOriginCloakingHeader=${previous_signin_origin_cloaking_header:-""}
@@ -35,11 +46,26 @@ TMP_PARAM_FILE=$(mktemp)
 echo "$PARAMETERS" | jq -r > "$TMP_PARAM_FILE"
 
 aws configure set region eu-west-2
-PARAMETERS_FILE=$TMP_PARAM_FILE ./provisioner.sh "${AWS_ACCOUNT}" auth-fe-cloudfront cloudfront-distribution v1.3.1
+PARAMETERS_FILE=$TMP_PARAM_FILE ./provisioner.sh "${AWS_ACCOUNT}" auth-fe-cloudfront cloudfront-distribution v1.6.0
 
-# # cloudfront monitoring, dependent on auth-fe-cloudfront
-# aws configure set region us-east-1
+CloudFrontDistributionID=$(aws cloudformation describe-stacks \
+    --stack-name auth-fe-cloudfront --region eu-west-2 |
+    jq -r '.Stacks[0].Outputs[] | select(.OutputKey | match("DistributionId")) | .OutputValue')
 
+# ------------------------------------------------------
+# auth-fe-cloudfront-monitoring
+#   deploys CloudFront Extended Monitoring configuration
+#   depends on: auth-fe-cloudfront
+# ------------------------------------------------------
+PARAMETERS_FILE="configuration/$AWS_ACCOUNT/auth-fe-cloudfront-monitoring/parameters.json"
+PARAMETERS=$(jq ". += [{\"ParameterKey\":\"CloudfrontDistribution\",\"ParameterValue\":\"${CloudFrontDistributionID}\"}] | tojson" -r "${PARAMETERS_FILE}")
+TMP_PARAM_FILE=$(mktemp)
+echo "$PARAMETERS" | jq -r > "$TMP_PARAM_FILE"
 
-# # reset
-# aws configure set region eu-west-2
+aws configure set region us-east-1
+PARAMETERS_FILE=$TMP_PARAM_FILE ./provisioner.sh "${AWS_ACCOUNT}" auth-fe-cloudfront-monitoring cloudfront-monitoring-alarm v2.0.0
+
+# -----
+# reset
+# -----
+aws configure set region eu-west-2
