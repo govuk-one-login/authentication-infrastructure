70a58ccd52186eafcdfc117797ae714f30484363
Monjurul Haque <monjurul.haque@digital.cabinet-office.gov.uk>
2024-07-25 17:07:15 +0100

    Documentation updated
    Issue: AUT-3365

--- provision-build.sh
+++ provision-build.sh
@@ -34,4 +34,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 || exit
 
 ./provisioner.sh "${AWS_ACCOUNT}" frontend-pipeline sam-deploy-pipeline v2.60.2
 
+# shallow clone templates from authentication repos
+./sync-dependencies.sh
+
 TEMPLATE_URL=file://authentication-frontend/cloudformation/domains/template.yaml ./provisioner.sh "${AWS_ACCOUNT}" dns-zones-and-records dns LATEST
