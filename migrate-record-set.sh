b2e61ef5a2de0a7748cc1122a86f4270174ed429
Pawankumar Kushwaha <pawankumar.kushwaha@digital.cabinet-office.gov.uk>
2024-12-04 12:57:01 +0000

    Added integraton json records file

--- migrate-record-set.sh
+++ migrate-record-set.sh
@@ -4,11 +4,11 @@ set -euo pipefail
 # Ensure we are in the directory of the script
 cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 || exit
 
-[ $# = 1 ] || { echo "Usage: $(basename "$0" .sh) newzoneid" >&1; exit 1; }
+[ $# = 2 ] || { echo "Usage: $(basename "$0" .sh) newzoneid env(production or integration )" >&2; exit 1; }
 
-export AWS_PROFILE=di-authentication-production-AWSAdministratorAccess
+export AWS_PROFILE=di-authentication-"${2}"-AWSAdministratorAccess
 aws sso login --profile "${AWS_PROFILE}"
 aws configure set region eu-west-2
 
 # Create the DNS record in new Zone id
-aws route53 change-resource-record-sets --hosted-zone-id "${1}" --change-batch file://dns-records-signin-prod.json
+aws route53 change-resource-record-sets --hosted-zone-id "${1}" --change-batch file://dns-records-signin-"${2}".json
