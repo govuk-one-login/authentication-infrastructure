84f2a91ce11434b7eea0af0f01ac6cddfdf5f938
Pawankumar Kushwaha <pawankumar.kushwaha@digital.cabinet-office.gov.uk>
2024-12-04 11:50:58 +0000

    AUT-3694: copy the DNS record for old Zone ID to new signin zone Id

--- migrate-record-set.sh
+++ migrate-record-set.sh
@@ -10,5 +10,5 @@ export AWS_PROFILE=di-authentication-production-AWSAdministratorAccess
 aws sso login --profile "${AWS_PROFILE}"
 aws configure set region eu-west-2
 
-# Created the DNS recird in new Zone id
+# Create the DNS record in new Zone id
 aws route53 change-resource-record-sets --hosted-zone-id "${1}" --change-batch file://dns-records-signin-prod.json
