79f41853b26f7f68f6cf55c85304c9d4ec2f43d4
Monjurul Haque <monjurul.haque@digital.cabinet-office.gov.uk>
2025-06-10 13:41:28 +0100

    Updated scripts

--- scripts/write_secrets.sh
+++ scripts/write_secrets.sh
@@ -15,14 +15,27 @@ if ! aws sts get-caller-identity &> /dev/null; then
 fi
 export AWS_REGION="eu-west-2"
 
+secrets="
+  account_intervention_service_uri |
+  ticf_cri_service_uri |
+  notify_api_key |
+  notify_test_destinations |
+  test_client_verify_email_otp |
+  test_client_verify_phone_number_otp |
+"
+
+# shellcheck disable=SC2162,SC2086
+while read -d"|" name; do
+  echo "reading /deploy/${ENVIRONMENT}/${name}"
+  export "${name}"="$(aws secretsmanager get-secret-value --secret-id "/deploy/${ENVIRONMENT}/${name}" --region "${AWS_REGION}" | jq -r '.SecretString')"
+done <<< ${secrets}
+
 case "${ENVIRONMENT}" in
   dev | build)
     interventions_api=$(aws apigateway get-rest-apis --query "items[?name=='${ENVIRONMENT}-di-interventions-api-stub'].[id]" --output text)
     ticf_cri_api=$(aws apigateway get-rest-apis --query "items[?name=='${ENVIRONMENT}-di-ticf-cri-stub'].[id]" --output text)
     ;;
   *)
-    interventions_api=$(aws secretsmanager get-secret-value --secret-id "/deploy/${ENVIRONMENT}/account_intervention_service_uri" --query 'SecretString' --output text)
-    ticf_cri_api=$(aws secretsmanager get-secret-value --secret-id "/deploy/${ENVIRONMENT}/ticf_cri_service_uri" --query 'SecretString' --output text)
     old_vpc_endpoint=$(aws ec2 describe-vpc-endpoints --filters 'Name=service-name,Values=com.amazonaws.eu-west-2.execute-api' --query 'VpcEndpoints[].VpcEndpointId' --output text)
     ;;
 esac
@@ -36,10 +49,13 @@ case "${ENVIRONMENT}" in
     ticf_cri_service_uri="https://${ticf_cri_api}-${vpc_endpoint}.execute-api.eu-west-2.amazonaws.com/${ENVIRONMENT}"
     ;;
   *)
-    account_intervention_service_uri=${interventions_api/"$old_vpc_endpoint"/"$vpc_endpoint"}
-    ticf_cri_service_uri=${ticf_cri_api/"$old_vpc_endpoint"/"$vpc_endpoint"}
+    account_intervention_service_uri=${account_intervention_service_uri/"$old_vpc_endpoint"/"$vpc_endpoint"}
+    ticf_cri_service_uri=${ticf_cri_service_uri/"$old_vpc_endpoint"/"$vpc_endpoint"}
     ;;
 esac
 
-aws secretsmanager create-secret --name "/deploy/${ENVIRONMENT}/account_intervention_service_uri" --secret-string "${account_intervention_service_uri}" --region "${AWS_REGION}"
-aws secretsmanager create-secret --name "/deploy/${ENVIRONMENT}/ticf_cri_service_uri" --secret-string "${ticf_cri_service_uri}" --region "${AWS_REGION}"
+# shellcheck disable=SC2162,SC2086
+while read -d"|" name; do
+  echo -n "."
+  aws secretsmanager create-secret --name "/deploy/${ENVIRONMENT}/${name}" --secret-string "${!name:- }" --region "${AWS_REGION}"
+done <<< ${secrets}
