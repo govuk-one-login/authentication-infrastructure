a4e0ce4ed32bb74e80e0740dd229b192c045e743
Pawankumar Kushwaha <pawankumar.kushwaha@digital.cabinet-office.gov.uk>
2025-08-14 12:44:26 +0100

    Smoketest SAM pipeline for DEV

--- provision-dev.sh
+++ provision-dev.sh
@@ -315,6 +315,18 @@ function provision_pipeline {
   PARAMETERS_FILE=$TMP_PARAM_FILE ./provisioner.sh "${AWS_ACCOUNT}" dev-smoke-test-pipeline sam-deploy-pipeline v2.87.0
 
 
+  # Dev Smoke test pipeline
+  PARAMETERS_FILE="configuration/$AWS_ACCOUNT/dev-smoke-test-pipeline/parameters.json"
+  PARAMETERS=$(jq ". += [
+                            {\"ParameterKey\":\"ContainerSignerKmsKeyArn\",\"ParameterValue\":\"${ContainerSignerKmsKeyArn}\"},
+                            {\"ParameterKey\":\"SigningProfileArn\",\"ParameterValue\":\"${SigningProfileArn}\"},
+                            {\"ParameterKey\":\"SigningProfileVersionArn\",\"ParameterValue\":\"${SigningProfileVersionArn}\"}
+                        ] | tojson" -r "${PARAMETERS_FILE}")
+
+  TMP_PARAM_FILE=$(mktemp)
+  echo "$PARAMETERS" | jq -r > "$TMP_PARAM_FILE"
+  PARAMETERS_FILE=$TMP_PARAM_FILE ./provisioner.sh "${AWS_ACCOUNT}" dev-smoke-test-pipeline sam-deploy-pipeline v2.76.0
+
   # dev Stubs api pipeline (ticf-cri-stub & interventions-api-stub)
   PARAMETERS_FILE="configuration/$AWS_ACCOUNT/dev-stubs-api-pipeline/parameters.json"
   PARAMETERS=$(jq ". += [
